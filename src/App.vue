1000dce8260a4ef9d711e9c582879f03b58ae153
ablasamer <34424826-ablasamer@users.noreply.replit.com>
2024-06-05 22:33:45 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+   
+  </header>
+
+  <RouterView />
+  <footer>
+    
+  </footer>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MobileMenu from '../components/MobileMenu.vue'
+</script>
+
+<template>
+  <main>
+    <MobileMenu />
+  </main>
+</template>
